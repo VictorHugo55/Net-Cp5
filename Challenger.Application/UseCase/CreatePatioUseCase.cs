24380293eba190f6b7c8e9ed9b14a52cfd28d457
VictorHugo55 <davichugopereira@gmail.com>
2025-09-11 12:00:49 -0300

    feat: atualizando o usecase de patio

--- Challenger.Application/UseCase/CreatePatioUseCase.cs
+++ Challenger.Application/UseCase/CreatePatioUseCase.cs
@@ -1,6 +1,37 @@
-﻿namespace Challenger.Application.UseCase;
+﻿using Challenger.Application.DTOs.Requests;
+using Challenger.Application.DTOs.Responses;
+using System.IO;
+
+namespace Challenger.Application.UseCase;
 
 public class CreatePatioUseCase
 {
-    
+    private readonly IPatioRepository _patioRepository;
+
+    public CreatePatioUseCase(IPatioRepository patioRepository)
+    {
+        _patioRepository = patioRepository;
+    }
+
+    public async Task<PatioResponse> Execute(PatioRequest request, string createdBy)
+    {
+        // Cria a entidade Patio com valores puros
+        var patio = new Patio(
+            request.Name,
+            request.Cidade,
+            request.Capacidade,
+            createdBy
+        );
+
+        // Salva no repositório
+        await _patioRepository.AddAsync(patio);
+
+        // Retorna DTO de resposta
+        return new PatioResponse(
+            patio.Id,
+            patio.Name,
+            patio.Cidade,
+            patio.Capacidade
+        );
+    }
 }
