6880cecb92d21381f10b22b119a03e0b5812f810
VictorHugo55 <davichugopereira@gmail.com>
2025-09-11 12:01:44 -0300

    feat: adiconando entidade audt e o enum modelo da moto

---
+++ Challenger.Domain/Entities/Audit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenger.Domain.Entities
+{
+    public abstract class Audit
+    {
+        public DateTime CreatedAt { get; protected set; }
+        public string CreatedBy { get; protected set; } = string.Empty;
+
+        public DateTime UpdatedAt { get; protected set; }
+        public string UpdatedBy { get; protected set; } = string.Empty;
+
+        // Método para inicializar a criação
+        protected void SetCreated(string user)
+        {
+            CreatedAt = DateTime.UtcNow;
+            CreatedBy = user;
+            UpdatedAt = CreatedAt;
+            UpdatedBy = CreatedBy;
+        }
+
+        // Método para atualizar auditoria
+        protected void SetUpdated(string user)
+        {
+            UpdatedAt = DateTime.UtcNow;
+            UpdatedBy = user;
+        }
+    }
+}
---
+++ Challenger.Domain/Enum/ModeloMoto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenger.Domain.Enum
+{
+    public enum ModeloMoto
+    {   
+        SPORT,
+        POP,
+        ELETRICA
+    }
+}
